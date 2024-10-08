2b176fb2826e30749d8dd406586b8e419f4ba505
MariamSofia <mariamsofiacanobakir@gmail.com>
2024-10-08 01:56:07 +0000

    Punto_E

--- RTA_SCRIPT_Examen_20241008/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_E.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+output_dir="repogit/UTNFRA_SO_1P2C_2024_-Cano_Bakir-/RTA_ARCHIVOS_Examen_20241008"
+mkdir -p "$output_dir"
+output_file="$output_dir/Filtro_Basico.txt"
 
-grep MemTotal /proc/meminfo | awk '{print $2, $3}' > repogit/UTNFRA_SO_1P2C_2024_-Cano_Bakir-/RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+memoria_total=$(grep MemTotal /proc/meminfo | awk '{print $2, $3}')
 
-sudo dmidecode -t chassis | grep Manufacturer | awk -F ': ' '{print $2}' >> repogit/UTNFRA_SO_1P2C_2024_-Cano_Bakir-/RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+fabricante_chasis=$(sudo dmidecode -t chassis | grep Manufacturer | awk -F ': ' '{print $2}')
+
+echo "Memoria Total: $memoria_total" > "$output_file"
+echo "Fabricante del Chasis: $fabricante_chasis" >> "$output_file"
+
 
